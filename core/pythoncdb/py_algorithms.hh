0a064d185cb7bf59f0bdc23833fb895ca0ee7a0d
Daniel Butter <dbutter@gmail.com>
2023-12-20 15:10:38 -0500

    Modify post_order algorithms to take an ExNode as an argument.
    pre_order algorithms are still unchanged.
    The post_order approach defines some new functions, but it would
    probably be simpler to override existing apply_algo functions to
    accomplish this.

--- core/pythoncdb/py_algorithms.hh
+++ core/pythoncdb/py_algorithms.hh
@@ -31,6 +31,24 @@ namespace cadabra {
 		return ex;
 		}
 
+	// Overload apply_algo_base to deal with ExNodes
+	template <class Algo>
+	Ex_ptr apply_algo_base_node(Algo& algo, Ex_ptr ex, ExNode& node, bool deep, bool repeat, unsigned int depth, bool pre_order=false)
+		{
+		Ex::iterator it = node.it;
+		if (ex->is_valid(it)) {
+			ProgressMonitor* pm = get_progress_monitor();
+			algo.set_progress_monitor(pm);
+			if(pre_order)
+				ex->update_state(algo.apply_pre_order(repeat));
+			else
+				ex->update_state(algo.apply_generic(it, deep, repeat, depth));
+			call_post_process(*get_kernel_from_scope(), ex);
+			}
+
+		return ex;
+		}
+
 	template <class Algo>
 	Ex_ptr apply_algo(Ex_ptr ex, bool deep, bool repeat, unsigned int depth)
 		{
@@ -60,6 +78,40 @@ namespace cadabra {
 		}
 
 
+	// Add new apply_algo_node functions to work with ExNodes
+	template <class Algo>
+	Ex_ptr apply_algo_node(ExNode& exnode, bool deep, bool repeat, unsigned int depth)
+		{
+		Ex_ptr ex = exnode.ex;
+		Algo algo(*get_kernel_from_scope(), *ex);
+		return apply_algo_base_node(algo, ex, exnode, deep, repeat, depth, false);
+		}
+
+	template <class Algo, typename Arg1>
+	Ex_ptr apply_algo_node(ExNode& exnode, Arg1 arg1, bool deep, bool repeat, unsigned int depth)
+		{
+		Ex_ptr ex = exnode.ex;
+		Algo algo(*get_kernel_from_scope(), *ex, arg1);
+		return apply_algo_base_node(algo, ex, exnode, deep, repeat, depth, false);
+		}
+
+	template <class Algo, typename Arg1, typename Arg2>
+	Ex_ptr apply_algo_node(ExNode& exnode, Arg1 arg1, Arg2 arg2, bool deep, bool repeat, unsigned int depth)
+		{
+		Ex_ptr ex = exnode.ex;
+		Algo algo(*get_kernel_from_scope(), *ex, arg1, arg2);
+		return apply_algo_base_node(algo, ex, exnode, deep, repeat, depth, false);
+		}
+
+	template <class Algo, typename Arg1, typename Arg2, typename Arg3>
+	Ex_ptr apply_algo_node(ExNode& exnode, Arg1 arg1, Arg2 arg2, Arg3 arg3, bool deep, bool repeat, unsigned int depth)
+		{
+		Ex_ptr ex = exnode.ex;
+		Algo algo(*get_kernel_from_scope(), *ex, arg1, arg2, arg3);
+		return apply_algo_base_node(algo, ex, exnode, deep, repeat, depth, false);
+		}
+
+
 	/// \ingroup pythoncore
 	///
 	/// Method to declare a Python function with variable number of
@@ -80,7 +132,34 @@ namespace cadabra {
 		      pybind11::arg("depth") = depth,
 		      pybind11::doc(read_manual("algorithms", name).c_str()),
 		      pybind11::return_value_policy::reference_internal);
+		
+		// Extend algorithms to work with ExNodes directly
+		m.def(name,
+		      &apply_algo_node<Algo, Args...>,
+		      pybind11::arg("node"),
+		      std::forward<PyArgs>(pyargs)...,
+		      pybind11::arg("deep") = deep,
+		      pybind11::arg("repeat") = repeat,
+		      pybind11::arg("depth") = depth,
+		      pybind11::doc(read_manual("algorithms", name).c_str()),
+		      pybind11::return_value_policy::reference_internal);
+
 		}
+	/*
+	template<class Algo, typename... Args, typename... PyArgs>
+	void def_algo_node(pybind11::module& m, const char* name, bool deep, bool repeat, unsigned int depth, PyArgs... pyargs)
+		{
+		m.def(name,
+		      &apply_algo_node<Algo, Args...>,
+		      pybind11::arg("node"),
+		      std::forward<PyArgs>(pyargs)...,
+		      pybind11::arg("deep") = deep,
+		      pybind11::arg("repeat") = repeat,
+		      pybind11::arg("depth") = depth,
+		      pybind11::doc(read_manual("algorithms", name).c_str()),
+		      pybind11::return_value_policy::reference_internal);
+		}
+	*/
 
 	template <class Algo>
 	Ex_ptr apply_algo_preorder(Ex_ptr ex, bool deep, bool repeat, unsigned int depth)
